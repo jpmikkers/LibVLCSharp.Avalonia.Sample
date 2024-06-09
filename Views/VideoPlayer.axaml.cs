d0f4ed22232dfb7a35ab9281a4f85945b1375afe
jpmikkers <jean.paul.mikkers@gmail.com>
2024-06-10 00:49:37 +0200

    LibVLCSharp 3.8.5

---
+++ Views/VideoPlayer.axaml.cs
@@ -0,0 +1,33 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using LibVLCSharp.Avalonia.Sample.ViewModels;
+
+namespace LibVLCSharp.Avalonia.Sample.Views
+{
+    public partial class VideoPlayer : UserControl
+    {
+        public VideoPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDataContextChanged(object sender, EventArgs e)
+        {
+            if(DataContext is MainWindowViewModel vm)
+            {
+                vm.Play();
+            }
+        }
+
+        private void VideoViewOnPointerEntered(object sender, PointerEventArgs e)
+        {
+            ControlsPanel.IsVisible = true;
+        }
+
+        private void VideoViewOnPointerExited(object sender, PointerEventArgs e)
+        {
+            ControlsPanel.IsVisible = false;
+        }
+    }
+}
